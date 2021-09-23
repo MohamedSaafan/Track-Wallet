34defa978d7a5ded0a56c861b552b9156da15cea
MohamedSaafan <201827254@std.sci.cu.edu.eg>
2021-09-23 10:24:53 +0200

    added the manage accounts page

--- src/features/pages/accounts/components/manage/index.tsx
+++ src/features/pages/accounts/components/manage/index.tsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import NavHeader from "../../../../../components/nav-header";
+import Account from "../account";
 import Styles from "./manage.module.scss";
 
 interface Props {}
@@ -6,8 +8,24 @@ interface Props {}
 const Manage: React.FC<Props> = (props) => {
   return (
     <div className='page'>
-      Manage goes here...
-      <Link to='/accounts'>Back</Link>
+      <NavHeader title='Manage Accounts' link='/accounts' />
+      <div className={Styles.accounts}>
+        <Account
+          isActive
+          accountAddress='[Track:1234...1344]'
+          accountName='Account#1'
+        />
+        <Account
+          isActive={false}
+          accountAddress='[Track:1234...1344]'
+          accountName='Account#1'
+        />
+        <Account
+          isActive={false}
+          accountAddress='[Track:1234...1344]'
+          accountName='Account#1'
+        />
+      </div>
     </div>
   );
 };
